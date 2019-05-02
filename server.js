0829dc5e171eec43f722d94ec5a9443e82e73138
Tia Rose <shortyshort789@aol.com>
2019-05-02 09:41:24 -0700

    added fetch function, added save prototype, added get location function

--- server.js
+++ server.js
@@ -67,20 +67,76 @@ function Events(data) {
   this.event_date = newDate;
   this.summary = data.summary;
 }
+//Static function
+Location.lookup = handler => {
+  const SQL = `SELECT * FROM locations WHERE search_query=$1;`;
+  const values = [handler.query];
+
+  return client.query(SQL, values)
+    .then(results => {
+      if(results.rowCount > 0){
+        handler.cacheHit(results);
+      }else{
+        handler.cacheMiss(results);
+      }
+    })
+    .catch(console.error);
+};
+
+Location.fetchLocation = (query) => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${data}&key=${process.env.GEOCODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(result => {
+      if(!result.body.results.length) throw 'No data';
+      let location = new Location(query, result.body.results[0]);
+      return location.save()
+        .then(result => {
+          location.id = result.rows[0].id;
+          return location;
+        });
+    });
+};
+
+Location.prototype.save = function(){
+  let SQL = `INSERT INTO locations 
+    (search_query, formatted_query, latitude, longitude)
+    VALUES ($1, $2 $3 $4)
+    RETURNING id;`;
+
+  let values = Object.values(this);
+
+  return client.query(SQL, values);
+};
 
 //--------------------------------
 // Route Callbacks
 //--------------------------------
 let searchCoords = (request, response) => {
-  const data = request.query.data;
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${data}&key=${process.env.GEOCODE_API_KEY}`;
-
-  return superagent.get(url)
-    .then(result => {
-      response.send(new Location(data, result.body.results[0]));
-    })
-    .catch(() => errorMessage());
+  const locationHandler = {
+    query: request.query.data,
+    cacheHit: results => {
+      console.log('Got the data');
+      response.send(results[0]);
+    },
+    cacheMiss: () => {
+      console.log('Fetching');
+      Location.fetchLocation(request.query.data)
+        .then(results => response.send(results));
+    }
+  };
+  Location.lookup(locationHandler);
 };
+
+
+// const data = request.query.data;
+// const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${data}&key=${process.env.GEOCODE_API_KEY}`;
+
+// return superagent.get(url)
+//   .then(result => {
+//     response.send(new Location(data, result.body.results[0]));
+//   })
+//   .catch(() => errorMessage());
 
 let searchWeather = (request, response) => {
   const data = request.query.data;
