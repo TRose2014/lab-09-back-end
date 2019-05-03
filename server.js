c93b01ba2e1d52146c29fb799e91c863b62a68b8
Tia Rose <shortyshort789@aol.com>
2019-05-03 15:48:47 -0700

    last attempt didn't work, add cors config object with origin - Adam

--- server.js
+++ server.js
@@ -17,14 +17,10 @@ const pg = require('pg');
 //--------------------------------
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.use(cors({
+  origin: 'http://127.0.0.1:8080',
+}));
+
 
 //--------------------------------
 // Database Config
